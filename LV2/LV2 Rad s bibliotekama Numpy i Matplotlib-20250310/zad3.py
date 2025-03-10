a7bc9da292f326530b02b2902e4191df6fe9fc75
IvanJaman <ivan.jaman7@gmail.com>
2025-03-10 20:29:14 +0100

    Finished zad3 lv2

--- LV2/LV2 Rad s bibliotekama Numpy i Matplotlib-20250310/zad3.py
+++ LV2/LV2 Rad s bibliotekama Numpy i Matplotlib-20250310/zad3.py
@@ -10,18 +10,47 @@ Skripta zadatak_3.py uˇ citava sliku ’road.jpg’. Manipulacijom odgovaraju´
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = plt.imread("road.jpg")
-img = img[:,:,0].copy()
+image = plt.imread("road.jpg")
 
-plt.figure()
-plt.imshow(img, cmap="gray")
-plt.show()
+def start():
+    plt.figure()
+    plt.imshow(image, cmap="gray")
+    plt.show()
 
 # a) dio
-brightImage = img + 50
-brightImage[brightImage > 255] = 255
-plt.figure()
-plt.imshow(brightImage, cmap="gray")
-plt.show()
+def brighten():
+    brightImage = image + 50
+    brightImage[brightImage > 255] = 255
+    plt.figure()
+    plt.imshow(brightImage, cmap="gray")
+    plt.show()
 
 # b) dio
+def crop():
+    x = image.shape[0]
+    y = image.shape[1]
+
+    startX = x // 2
+    startY = y // 2
+    croppedImage = image[startX:, startY:]
+
+    plt.figure()
+    plt.imshow(croppedImage, cmap="gray")
+    plt.show()
+
+# c) dio
+def rotate():
+    rotatedImage = np.rot90(image, k=1, axes=(1, 0))
+
+    plt.figure()
+    plt.imshow(rotatedImage, cmap="gray")
+    plt.show()
+
+# d) dio
+def mirror():
+    mirrorImage = np.fliplr(image)
+
+    plt.figure()
+    plt.imshow(mirrorImage, cmap="gray")
+    plt.show()
+
